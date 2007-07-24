940336a1c4d9f51736c096ccbcf83a8a994a7cd2
ianb <ianb@71705e6d-80f1-0310-9d5e-e4e1c580a33d>
2007-07-24 02:27:04 +0000

    initial layout
    git-svn-id: http://svn.pythonpaste.org/Paste/WSGITest/trunk@6737 71705e6d-80f1-0310-9d5e-e4e1c580a33d

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9'
+
+setup(name='WSGITest',
+      version=version,
+      description="Helper to test WSGI applications",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='wsgi test unit tests web',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='http://pythonpaste.org/wsgitest/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
