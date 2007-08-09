eea4010baf16e24d003afc9fc415c3c802c2c904
ianb <ianb@71705e6d-80f1-0310-9d5e-e4e1c580a33d>
2007-08-09 03:37:54 +0000

    updated docs
    git-svn-id: http://svn.pythonpaste.org/Paste/WSGITest/trunk@6803 71705e6d-80f1-0310-9d5e-e4e1c580a33d

--- setup.py
+++ setup.py
@@ -7,8 +7,22 @@ setup(name='WSGITest',
       version=version,
       description="Helper to test WSGI applications",
       long_description="""\
+This wraps any WSGI application and makes it easy to send test
+requests to that application, without starting up an HTTP server.
+
+This provides convenient full-stack testing of applications written
+with any WSGI-compatible framework.
+
+This is based on ``paste.fixture.TestApp``.
 """,
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Paste",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+      ],
       keywords='wsgi test unit tests web',
       author='Ian Bicking',
       author_email='ianb@colorstudy.com',
@@ -24,4 +38,8 @@ setup(name='WSGITest',
       dependency_links=[
         'http://svn.pythonpaste.org/Paste/WebOb/trunk#egg=WebOb-dev',
       ],
+      entry_points="""
+      [paste.app_factory]
+      debug = wsgitest.debugapp:make_debug_app
+      """,
       )
